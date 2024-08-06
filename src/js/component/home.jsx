390c05e07e3dc4b419c4c934c501b25edb937ef3
CHCHALLE <Chrischalle@yahoo.com>
2024-08-06 01:28:28 +0000

    added fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,57 +1,134 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-const Home =() =>{
+// Include images into your bundle
+const Home = () => {
+	const [todos, setTodos] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+	const [editTodoId, setEditTodoId] = useState(null);
+	const [editValue, setEditValue] = useState("");
 
-	const[todos, setTodos]=useState([]);
-	const[inputValue, setInputValue]=useState("");
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+
+	const fetchUsers = () => {
+		fetch('https://playground.4geeks.com/todo/users/chchalle')
+			.then((res) => res.json())
+			.then((user) => {
+				setTodos(user.todos);
+			});
+	};
+
+	const deleteTodo = (todoId) => {
+		fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {
+			method: 'DELETE',
+			headers: { 'Content-Type': 'application/json' }
+		})
+			.then(() => fetchUsers())
+			.catch((error) => {
+				console.error(error);
+			});
+	};
+
+	const addTodo = () => {
+		const newToDo = { label: inputValue, is_done: false };
+
+		fetch('https://playground.4geeks.com/todo/todos/chchalle', {
+			method: 'POST',
+			body: JSON.stringify(newToDo),
+			headers: { 'Content-Type': 'application/json' }
+		})
+			.then(() => {
+				fetchUsers();
+				setInputValue("");
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
+
+	const updateTodo = (todoId) => {
+		const updatedTodo = { label: editValue, is_done: false };
+
+		fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {
+			method: 'PUT',
+			body: JSON.stringify(updatedTodo),
+			headers: { 'Content-Type': 'application/json' }
+		})
+			.then(() => {
+				fetchUsers();
+				setEditTodoId(null);
+				setEditValue("");
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
+
+	const handleKeyDown = (event, todoId) => {
+		if (event.key === 'Enter') {
+			updateTodo(todoId);
+		}
+	};
+
 	return (
 		<div className="container">
 			<h1>My Todos</h1>
-			<div class="container">
+			<div className="container">
 				<ul>
-					<li class="row"><input 
-					class="col-12"
-					type="text" 
-					value= {inputValue}
-					onChange=
-					{
-						(event)=> 
-						{
-							setInputValue(event.target.value);
-						}
-					}					
-			onKeyDown=
-				{
-					(event)=>
-					{
-						const key= event.key;
-						if (key==='Enter')
-						{
-							setTodos(todos.concat(inputValue));
-							setInputValue("");
-						}
-					}
-				}
-					
-				placeholder="Next Task"></input>
-				</li>
-					{todos.map((task, index)=>(
-					<li class="row">
-						<div class="col-11">{task}</div>
-					
-					<i id="icon" class="fas fa-trash-alt col-1 float-right" 
-					onClick={() => setTodos(todos.filter((t,currentIndex)=> index!=currentIndex))}
-					></i></li>
-					))
-					}
+					<li className="row">
+						<input
+							className="col-12"
+							type="text"
+							value={inputValue}
+							onChange={(event) => setInputValue(event.target.value)}
+							onKeyDown={(event) => {
+								if (event.key === 'Enter') {
+									addTodo();
+								}
+							}}
+							placeholder="Next Task"
+						/>
+					</li>
+					{todos.map((todo) => (
+						<li className="row" key={todo.id}>
+							{editTodoId === todo.id ? (
+								<input
+									type="text"
+									value={editValue}
+									className="col-11"
+									onChange={(event) => setEditValue(event.target.value)}
+									onKeyDown={(event) => handleKeyDown(event, todo.id)}
+									onBlur={() => {
+										// Save changes when input loses focus
+										updateTodo(todo.id);
+									}}
+								/>
+							) : (
+								<input
+									type="text"
+									value={todo.label}
+									className="col-11"
+									readOnly
+									onClick={() => {
+										setEditTodoId(todo.id);
+										setEditValue(todo.label);
+									}}
+								/>
+							)}
+							<i
+								id="icon"
+								className="fas fa-trash-alt col-1 float-right"
+								onClick={() => deleteTodo(todo.id)}
+							></i>
+						</li>
+					))}
 				</ul>
-				</div>
-				<div>{todos.length}tags</div>
-				<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+			</div>
+			<div>{todos.length} tasks</div>
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 		</div>
-	
-	)
+	);
 };
 
 export default Home;
